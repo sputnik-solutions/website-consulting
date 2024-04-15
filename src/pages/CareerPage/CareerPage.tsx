0b5dc930af9216d473372d0a88c46b20ebec27b4
Ivan Besedin <ivanbesedin.05@gmail.com>
2024-04-15 13:41:28 +0300

    lase section in servs and table left

--- src/pages/CareerPage/CareerPage.tsx
+++ src/pages/CareerPage/CareerPage.tsx
@@ -1,4 +1,16 @@
 import {motion} from 'framer-motion'
+import styles from './CareerPage.module.scss'
+
+import top_left from 'assets/icons/home_page/top_left.svg'
+import top_right from 'assets/icons/home_page/top_right.svg'
+import { Btn, Container } from 'shared'
+
+import icon12 from 'assets/icons/other_pages/icon12.svg'
+import icon13 from 'assets/icons/other_pages/icon13.svg'
+import icon14 from 'assets/icons/other_pages/icon14.svg'
+import icon22 from 'assets/icons/other_pages/icon22.svg'
+import icon23 from 'assets/icons/other_pages/icon23.svg'
+import icon24 from 'assets/icons/other_pages/icon24.svg'
 
 const CareerPage = () => {
 	return (
@@ -8,7 +20,111 @@ const CareerPage = () => {
 			exit={{opacity: 0}}
 			transition={{duration: 0.2}}
 		>
-			<h1>КАРЬЕРА</h1>
+			<div className={styles.page}>
+				<div className={styles.cap}>
+					<img src={top_left} alt="" className={styles.cap__left}/>
+					<img src={top_right} alt="" className={styles.cap__right}/>
+
+					<p className={styles.cap__title}>КАРЬЕРА</p>
+					<div className={styles.cap__descr}>Краткое описание сайта. Слоган. То, что может получить пользователь на <br />
+					сайте и к каким разделам может перейти.</div>
+					<Btn className={styles.cap__btn}>КНОПКА для перехода</Btn>
+				</div>
+
+				<div style={{position: 'relative'}}>
+					<img src={icon12} alt="" className={styles.icon12}/>
+					<img src={icon22} alt="" className={styles.icon22}/>
+					<Container className={styles.vacancy}>
+						<>
+							<div className={styles.vacancy__left}>
+								<p className={styles.vacancy__title}>Название вакансии</p>
+								<p className={styles.vacancy__descr}>Подробное описание вакансии. Преимущества и выгода для пользователя. <br /><br />
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl rhoncus mattis rhon- cus urna neque
+								</p>
+								<div className={styles.vacancy__advantages}>
+									<div className={styles.advantages__item}>
+										<div className={styles.advantages__dot}></div>
+										<div className={styles.advantages__title}>Преимущество</div>
+										<div className={styles.advantages__text}>Описание преимущества компании. Выгоды, доп услуги и другое.</div>
+									</div>
+									<div className={styles.advantages__item}>
+										<div className={styles.advantages__dot} style={{background: 'linear-gradient(90deg, #00d980 0%, #00c4a2 100%)'}}></div>
+										<div className={styles.advantages__title}>Преимущество</div>
+										<div className={styles.advantages__text}>Описание преимущества компании. Выгоды, доп услуги и другое.</div>
+									</div>
+								</div>
+							</div>
+							<div className={styles.vacancy__right}>
+								<div className={styles.vacancy__img} style={{height: '33%'}}>Место для фото</div>
+								<div className={styles.vacancy__img} style={{height: '65%'}}>Место для фото</div>
+							</div>
+						</>
+					</Container>
+				</div>
+
+				<div style={{position: 'relative'}}>
+					<img src={icon13} alt="" className={styles.icon13}/>
+					<img src={icon23} alt="" className={styles.icon23}/>
+					<Container className={styles.vacancy}>
+						<>
+							<div className={styles.vacancy__left}>
+								<p className={styles.vacancy__title}>Название вакансии</p>
+								<p className={styles.vacancy__descr}>Подробное описание вакансии. Преимущества и выгода для пользователя. <br /><br />
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl rhoncus mattis rhon- cus urna neque
+								</p>
+								<div className={styles.vacancy__advantages}>
+									<div className={styles.advantages__item}>
+										<div className={styles.advantages__dot} style={{backgroundColor: '#008aff'}}></div>
+										<div className={styles.advantages__title}>Преимущество</div>
+										<div className={styles.advantages__text}>Описание преимущества компании. Выгоды, доп услуги и другое.</div>
+									</div>
+									<div className={styles.advantages__item}>
+										<div className={styles.advantages__dot} style={{backgroundColor: '#ff005c'}}></div>
+										<div className={styles.advantages__title}>Преимущество</div>
+										<div className={styles.advantages__text}>Описание преимущества компании. Выгоды, доп услуги и другое.</div>
+									</div>
+								</div>
+							</div>
+							<div className={styles.vacancy__right}>
+								<div className={styles.vacancy__img} style={{height: '33%'}}>Место для фото</div>
+								<div className={styles.vacancy__img} style={{height: '65%'}}>Место для фото</div>
+							</div>
+						</>
+					</Container>
+				</div>
+
+				<div style={{position: 'relative'}}>
+					<img src={icon14} alt="" className={styles.icon14}/>
+					<img src={icon24} alt="" className={styles.icon24}/>
+					<Container className={styles.vacancy}>
+						<>
+							<div className={styles.vacancy__left}>
+								<p className={styles.vacancy__title}>Название вакансии</p>
+								<p className={styles.vacancy__descr}>Подробное описание вакансии. Преимущества и выгода для пользователя. <br /><br />
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl rhoncus mattis rhon- cus urna neque
+								</p>
+								<div className={styles.vacancy__advantages}>
+									<div className={styles.advantages__item}>
+										<div className={styles.advantages__dot} style={{backgroundColor: '#ff005c'}}></div>
+										<div className={styles.advantages__title}>Преимущество</div>
+										<div className={styles.advantages__text}>Описание преимущества компании. Выгоды, доп услуги и другое.</div>
+									</div>
+									<div className={styles.advantages__item}>
+										<div className={styles.advantages__dot} style={{backgroundColor: '#6800de'}}></div>
+										<div className={styles.advantages__title}>Преимущество</div>
+										<div className={styles.advantages__text}>Описание преимущества компании. Выгоды, доп услуги и другое.</div>
+									</div>
+								</div>
+							</div>
+							<div className={styles.vacancy__right}>
+								<div className={styles.vacancy__img} style={{height: '33%'}}>Место для фото</div>
+								<div className={styles.vacancy__img} style={{height: '65%'}}>Место для фото</div>
+							</div>
+						</>
+					</Container>
+				</div>
+
+			</div>
 		</motion.div>
 	)
 }
--- src/pages/ServicesPage/ServicesPage.tsx
+++ src/pages/ServicesPage/ServicesPage.tsx
@@ -1,6 +1,11 @@
 
 import {motion} from 'framer-motion'
+import styles from './ServicesPage.module.scss'
+import { Container } from 'shared'
 
+import sputnik from 'assets/icons/home_page/sputnik.svg'
+import benefit_img from 'assets/img/services/benefit_img.png'
+import arrow from 'assets/img/services/arrow.svg'
 const ServicesPage = () => {
 	return (
 		<motion.div
@@ -9,7 +14,140 @@ const ServicesPage = () => {
 			exit={{opacity: 0}}
 			transition={{duration: 0.2}}
 		>
-			<h1>УСЛУГИ</h1>
+			<div className={styles.page}>
+				<div style={{position: 'relative'}}>
+					<Container className={styles.cap}>
+						<>
+							<p className={styles.cap__title}>РЕСТОРАННЫЙ БИЗНЕС</p>
+							<div className={styles.cap__descr}>
+								<p>Подробное описание услуги, преимущества, плюсы в работе. Выгода для клиента.</p>
+								<p>Hendrerit gravida rutrum quisque non tellus. Rhoncus mattis rhoncus urna neque viverra. Elementum facilisis leo vel fringilla est. In fermentum et sollicitudin ac orci. Dui faucibus in ornare quam viverra. Portti-tor lacus luctus accumsan tortor posuere. Turpis nunc eget lorem dolor sed.</p>
+							</div>
+						</>
+					</Container>
+				</div>
+
+				<div style={{position: 'relative'}}>
+					<Container className={styles.descr}>
+						<>	
+							<div className={styles.descr__block}>
+								<p className={styles.descr__title}>Описание </p>
+								<p className={styles.descr__subtitle}>Краткая информация о статистике и подобном</p>
+								<p className={styles.descr__text}>Подробная информация. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl rhoncus mattis rhoncus urna neque. Vitae sapien pellentesque habitant morbi tristique senectus et. Dolor sit amet consectetur adipiscing elit ut aliquam purus sit. Adipiscing diam donec adipiscing tristique. Hendrerit gravida rutrum quisque non tellus. </p>
+							</div>
+							<div className={styles.descr__img}>Место для фото</div>
+							<div className={styles.descr__img}>Место для фото</div>
+							<div className={styles.descr__block}>
+								<p className={styles.descr__title}>Описание </p>
+								<p className={styles.descr__subtitle}>Краткая информация о статистике и подобном</p>
+								<p className={styles.descr__text}>Подробная информация. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl rhoncus mattis rhoncus urna neque. Vitae sapien pellentesque habitant morbi tristique senectus et. Dolor sit amet consectetur adipiscing elit ut aliquam purus sit. Adipiscing diam donec adipiscing tristique. Hendrerit gravida rutrum quisque non tellus. </p>
+							</div>
+							
+						</>
+					</Container>
+				</div>
+
+				<div style={{position: 'relative'}} className={styles.numbers}>
+					<div className={styles.numbers__wrapper}>
+						<div className={styles.numbers__title}>
+							<p>Цифры</p>
+							<img src={sputnik} alt="" />
+						</div>
+						<p className={styles.numbers__subtitle}>Информация о разделе. Lorem  <br /> ipsum dolor sit amet, consectetur ad- <br /> ipiscing elit, sed do eiusmod.</p>
+					</div>
+					<Container className={styles.numbers__examples}>
+						<>
+						<div className={styles.numbers__line}></div>
+
+						<div className={styles.numbers__example}>
+							<p className={styles.numbers__example__title}>12.000$</p>
+							<p className={styles.numbers__example__descr}>Статистическая информация. Описание цифр. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+						</div>
+
+						<div className={styles.numbers__line}></div>
+
+						<div className={styles.numbers__example}>
+							<p className={styles.numbers__example__title}>2.000$</p>
+							<p className={styles.numbers__example__descr}>Статистическая информация. Описание цифр. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+						</div>
+
+						<div className={styles.numbers__line}></div>
+
+						<div className={styles.numbers__example}>
+							<p className={styles.numbers__example__title}>35%</p>
+							<p className={styles.numbers__example__descr}>Статистическая информация. Описание цифр. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+						</div>
+
+						<div className={styles.numbers__line}></div>
+						</>
+					</Container>
+
+				</div>
+				
+				<div style={{position: 'relative'}}>
+					<Container className={styles.motivation}>
+						<>
+							<div style={{display: 'flex'}}>
+								<p className={styles.motivation__title}>НАЗВАНИЕ РАЗДЕЛА</p>
+								<div className={styles.motivation__img}>
+									<div className={styles.motivation__img_pink}></div>
+								</div>
+								<div className={styles.motivation__slogan}>
+									Кратная информация о разделе. Слоган. Мотивирующая надпись. <br />
+									Дополнительные услуги и плюсы сотрудничества. 
+								</div>
+							</div>
+							<div className={styles.motivation__wrapper}>
+								<div className={styles.motivation__benefits}>
+									<div className={styles.motivation__benefits__item}>
+										<img src={benefit_img} alt="" />
+										<p className={styles.motivation__benefits__text}>Преимущество</p>
+									</div>
+									<div className={styles.motivation__benefits__item}>
+										<img src={benefit_img} alt="" />
+										<p className={styles.motivation__benefits__text}>Преимущество</p>
+									</div>
+									<div className={styles.motivation__benefits__item}>
+										<img src={benefit_img} alt="" />
+										<p className={styles.motivation__benefits__text}>Преимущество</p>
+									</div>
+									<div className={styles.motivation__benefits__item}>
+										<img src={benefit_img} alt="" />
+										<p className={styles.motivation__benefits__text}>Преимущество</p>
+									</div>
+									<p className={styles.motivation__benefits__descr}>
+										Краткоеописание.Loremipsumdolorsitamet,consectetuerad- <br /> 
+										ipiscingelit,seddiamnonummynibheuismodtinciduntutlaoreet <br />
+										doloremagnaaliquameratvolutpat.Utwisienimadminim <br />
+										veniam,quisnostrudexercitationullamcorpersuscipitlobortisnisl <br />
+										ut aliquip ex ea commodo consequat. <br />
+									</p>
+									<button className={styles.motivation__benefits__btn}>КНОПКА ДЛЯ ПЕРЕХОДА</button>
+								</div>
+
+								<div className={styles.motivation__btn}>
+									<p className={styles.motivation__btn__title}>МОТИВИРУЮЩИЙ СЛОГАН</p>
+									<p className={styles.motivation__btn__descr}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed 
+									diam nonummy nibh euismod tincidunt ut laoreet dolore 
+									</p>
+									<div className={styles.motivation__btn__circle}>
+										<div style={{backgroundColor: 'white', height: '150px', width: '150px', borderRadius: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+											<img src={arrow} alt="" />
+										</div>
+									</div>
+								</div>
+							</div>
+						</>
+					</Container>
+
+				</div>
+
+				<div style={{position: 'relative'}}>
+					<Container className={styles.last}>
+
+					</Container>
+				</div>
+			</div>
 		</motion.div>
 	)
 }
